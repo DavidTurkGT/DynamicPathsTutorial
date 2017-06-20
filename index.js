efa250b902987b4d6a38614caf5b0f19066a0e45
DavidTurkGT <dlturk747@gmail.com>
2017-06-20 09:35:12 -0400

    Added dynamic path on /:first

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-  res.send("Your app has started.  Yay!")
+  res.send("Your app has started.  Yay!");
 });
+
+app.get("/:first", function(req, res){
+  res.send("Your first name is " + req.params.first + ".");
+});
+
+app.get("/:first/:last", function(req, res){
+  let firstName = req.params.first,
+      lastName = req.params.last;
+  res.send();
+});
+
 
 app.listen(3000, function(){
   console.log("Server up and running on localhost:3000");
