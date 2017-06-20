4147c149a04d2fa0686162c813c2ec867d4d37b9
DavidTurkGT <dlturk747@gmail.com>
2017-06-20 09:43:21 -0400

    Added third dynamic path at /:first/:last/:age

--- index.js
+++ index.js
@@ -9,13 +9,18 @@ app.get("/:first", function(req, res){
   res.send("Your first name is " + req.params.first + ".");
 });
 
-
 app.get("/:first/:last", function(req, res){
   let firstName = req.params.first,
       lastName = req.params.last;
   res.send("Your full name is " + firstName + " " + lastName + ".");
 });
 
+app.get("/:first/:last/:age", function(req, res){
+  let firstName = req.params.first,
+      lastName = req.params.last,
+      age = req.params.age;
+  res.send("Your full name is " + firstName + " " + lastName + " and you are " + age + " years old");
+});
 
 app.listen(3000, function(){
   console.log("Server up and running on localhost:3000");
